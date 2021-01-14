eb7f299652c3970fa772703809b7628e8e5c17f6
hhaggang22 <20181018@sungshin.ac.kr>
2021-01-14 12:32:53 +0900

    post_list read

--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render,HttpResponse
+from .models import Post
 
 def post_list(request):
-    return HttpResponse('글 리스트')
+    #return HttpResponse('글 리스트')
+    posts = Post.objects.all() #models.py에서 만든 post 클래스를 모두 가져와서 posts에 넣는다.
+    ctx = {'posts' : posts}
+
+    return render(request, template_name = 'posts/list.html', context = ctx)
